01de03137368a40386e2c82e3f611e4f1a556a55
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-24 20:53:54 -0500

    add initial const

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-'use strict';
+"use strict";
+const buttonRoll = document.querySelector(".btn--roll");
+const buttonHold = document.querySelector(".btn--hold");
+const buttonNew = document.querySelector(".btn--new");
+console.log(buttonHold, buttonNew, buttonRoll);
+//  added buttons const
