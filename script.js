11edf9034efd6289291a883cce64b95794993d38
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-25 20:02:27 -0500

    add initial code

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
-const buttonRoll = document.querySelector(".btn--roll");
-const buttonHold = document.querySelector(".btn--hold");
-const buttonNew = document.querySelector(".btn--new");
-console.log(buttonHold, buttonNew, buttonRoll);
-//  added buttons const
+// selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+//  starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
