b6ee6ffe11fab690149b2049fdebafb79327d24a
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-25 20:31:16 -0500

    add dice roll function

--- script.js
+++ script.js
@@ -3,7 +3,20 @@
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
+const btnnew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 //  starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+// rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2.display dice
+
+  diceEl.classList.remove("hidden");
+  diceEl.src = `/images/dice-${dice}.png`;
+  // 3.check for rolled 1: if true, switch player
+});
