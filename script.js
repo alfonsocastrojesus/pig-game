a9c43d85af38098dd709c325f4c2457879cedc91
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-28 18:26:02 -0500

    add new game logic

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const btnHold = document.querySelector(".btn--hold");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -27,6 +28,17 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
+btnnew.addEventListener("click", function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0.classList.add("player--active");
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--active");
+});
 
 // rolling dice functionality
 btnRoll.addEventListener("click", function () {
