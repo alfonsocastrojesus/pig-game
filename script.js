5296b6077f971004bac8236593c2baa1acc39cd3
castro_alfonso <alfonso.je.castro@gmail.com>
2025-09-27 23:10:02 -0500

    only hold btn left

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 "use strict";
 // selecting elements
-const score0El = document.getElementById("score--0");
-const score1El = document.getElementById("score--1");
+let score0El = document.getElementById("score--0");
+let score1El = document.getElementById("score--1");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
 const btnnew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
 //  starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -31,6 +34,12 @@ btnRoll.addEventListener("click", function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
+
     // switch player
   }
 });
